e02a73eda9f3ac787f6a490cfb23a4a28fdc6157
slizz <cadenneary@gmail.com>
2022-08-02 13:01:06 -0500

    Push 3

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
-// template_30b8fri
-// service_ekkgeii
-// ytuAGmvTJjnAwCVMQ
+let isModalOpen = false
+let contrastToggle = false
+const scaleFactor = 1 / 20
 
-// function contact(event) {
-//   event.preventDefault();
-//   const loading = document.querySelector('.modal__overlay--loading');
-//   const success = document.querySelector('.modal__overlay--success');
-//   loading.classList += " modal__overlay--visible";
-//   emailjs
-//     .sendForm(
-//       'service_ekkgeii',
-//       'template_30b8fri',
-//       event.target,
-//       'ytuAGmvTJjnAwCVMQ'
-//     )
-//     .then(() => {
-//       loading.classList.remove("modal__overlay--visible");
-//       success.classList += " modal__overlay--visble";
-//     })
-//     .catch(() => {
-//       loading.classList.remove("modal__overlay--visible");
-//       alert(
-//         "The email service is currently unavailable. Please contact me directly at CadenNeary9@gmail.com"
-//       );
-//     });
-// }
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape")
+  const x = event.clientX * scaleFactor
+  const y = event.clientY * scaleFactor
 
+  for (let i = 0; i <shapes.length ; ++i) {
+    const isOdd = i % 2 !== 0
+    const boolInt = isOdd ? -1 : 1
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+  }
+}
+
+function toggleContrast () {
+  contrastToggle = !contrastToggle
+  if (contrastToggle) {
+    document.body.classList += " dark-theme"
+  }
+  else {
+    document.body.classList.remove("dark-theme")
+  }
+}
 
 function contact(event) {
     event.preventDefault();
@@ -55,7 +52,7 @@ function contact(event) {
     document.body.classList += " modal--open"
   }
 
-let isModalOpen = false
+
 function toggleModal() {
     if (isModalOpen) {
       isModalOpen = false;
